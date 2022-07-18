ea77e52fd46ab7f17772e9e4d9bba74d68b1095d
Vadim Bayramov <bayramovva@gmail.com>
2022-07-19 01:49:26 +0300

    Add Zadacha23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index <= num)
+{
+    double result = Math.Pow(index, 3);
+    Console.WriteLine($"{index} в кубе = {result}");
+    index++;
+}
